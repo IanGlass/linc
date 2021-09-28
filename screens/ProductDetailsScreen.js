0b1e35aae97e1d222e925d083e394afdc66dfcd3
IanGlass <37228869+IanGlass@users.noreply.github.com>
2021-09-28 22:55:44 +1300

    Pad out ProductDetails

--- screens/ProductDetailsScreen.js
+++ screens/ProductDetailsScreen.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import _ from 'lodash';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image, Button, StyleSheet } from 'react-native';
 
 import PRODUCTS from '../data/dummy-data';
 
@@ -15,9 +15,20 @@ const ProductDetailsScreen = ({ navigation }) => {
 
   return (
     <View>
-      
+      <Image
+        style={styles.image}
+        source={{ uri: product.imageUrl }}
+      />
+      <View style={styles.metaContainer}>
+        <Text>${product.price}</Text>
+      </View>
+      <View style={styles.metaContainer}>
+        <Text>{product.description}</Text>
+      </View>
+      <View style={styles.button}>
+        <Button title="Add to Cart" />      
+      </View>
     </View>
-    <Text>{product.title}</Text>
   );
 };
 
@@ -26,7 +37,18 @@ ProductDetailsScreen.navigationOptions = (navigationData) => ({
 });
 
 const styles = StyleSheet.create({
-
+  button: {
+    marginVertical: 5,
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: 200
+  },
+  metaContainer: {
+    marginVertical: 5,
+    alignItems: 'center'
+  }
 });
 
 export default ProductDetailsScreen;
