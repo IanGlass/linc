df0149db7abbf97691295e61fec81bda7580b5b3
IanGlass <37228869+IanGlass@users.noreply.github.com>
2021-10-04 09:51:53 +1300

    Pre-load fonts

--- App.js
+++ App.js
@@ -1,10 +1,29 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useState } from 'react';
+import * as Font from 'expo-font';
+import AppLoading from 'expo-app-loading';
 import { NavigationContainer } from '@react-navigation/native';
 
 import Navigator from './navigation/Navigator';
 
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+}
+
 export default function App() {
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return <AppLoading
+      startAsync={fetchFonts}
+      onError={(error) => console.log(error)}
+      onFinish={() => setDataLoaded(true)}
+    />
+  }
+
   return (
     <NavigationContainer>
       <Navigator />
