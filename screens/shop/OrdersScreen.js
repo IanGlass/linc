cfc90b2f164b0c8dcff4ed63f1a590a995238a75
IanGlass <37228869+IanGlass@users.noreply.github.com>
2021-10-26 11:07:21 +1300

    Show basic order details

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Text, FlatList } from 'react-native';
 
+import OrderItem from '../../components/shop/OrderItem';
+
 const OrdersScreen = ({ orders }) => {
   return (
     <FlatList
       data={orders}
-      renderItem={({item}) => <Text>{item.totalAmount}</Text>}
+      renderItem={({ item }) => (
+        <OrderItem totalAmount={item.totalAmount} date={item.readableDate} />
+      )}
     />
   );
 };
 
 const mapStateToProps = state => ({
-  orders: state.orders
+  orders: state.orders.orders
 });
 
 export default connect(mapStateToProps, undefined)(OrdersScreen);
